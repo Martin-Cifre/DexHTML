0105215b558045d739b64bf6c50a6682d87545ad
Martin-Cifre <martin.cifre@outlook.com>
2024-01-11 19:41:17 -0300

    proyecto de practica de Pokedex en HTML

---
+++ main.js
@@ -0,0 +1,10 @@
+var btnFiltro = document.querySelector(".filtroIcono");
+
+btnFiltro.addEventListener('click', () => {
+    const contenedorFiltros = document.querySelector(".contenedorFiltros");
+
+    contenedorFiltros.classList.toggle("activo");
+
+})
+
+//56:52
